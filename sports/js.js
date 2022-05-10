4b07090f4a75d415a0e6d4739fe0dc395e9971e4
JosephITPJ <105132473+JosephITPJ@users.noreply.github.com>
2022-05-10 21:51:51 +0200

    Add files via upload

---
+++ sports/js.js
@@ -0,0 +1,18 @@
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "تم الشراء.";
+}
+</script>
+</head>
+<body>
+
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">شراء.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html> 
